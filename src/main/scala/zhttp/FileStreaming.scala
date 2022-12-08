dcc7d46c89f04471f1c19343561de514cb016222
Andreas Pauley <apauley@elmar.nl>
2022-12-08 22:47:37 +0100

    Queue stream

--- src/main/scala/zhttp/FileStreaming.scala
+++ src/main/scala/zhttp/FileStreaming.scala
@@ -11,20 +11,25 @@ import java.nio.file.Paths
 
 object FileStreaming extends ZIOAppDefault {
 
+  val testVideo = "src/main/resources/TestVideoFile.m4v"
+
   // Create HTTP route
   val app = Http.collectHttp[Request] {
     case Method.GET -> !! / "health" => Http.ok
 
     // Read the file as ZStream
     // Uses the blocking version of ZStream.fromFile
-    case Method.GET -> !! / "blocking" => Http.fromStream(ZStream.fromPath(Paths.get("src/main/resources/TestVideoFile.m4v")))
-    case Method.GET -> !! / "receive" => Http.fromStream(StreamHelpers.tickStream(100L, 100))
+    case Method.GET -> !! / "blocking" => Http.fromStream(ZStream.fromPath(Paths.get(testVideo)))
+    case Method.GET -> !! / "filezio" => Http.fromFileZIO(ZIO.succeed(new File(testVideo)))
+
+    case Method.GET -> !! / "receive" => Http.fromStream(StreamHelpers.tickStream(1000L, 10))
+    case Method.GET -> !! / "q" => Http.fromStream(StreamHelpers.sampleQStream("Hello bleep"))
 
 
     // Uses netty's capability to write file content to the Channel
     // Content-type response headers are automatically identified and added
     // Adds content-length header and does not use Chunked transfer encoding
-    case Method.GET -> !! / "video" => Http.fromFile(new File("src/main/resources/TestVideoFile.m4v"))
+    case Method.GET -> !! / "video" => Http.fromFile(new File(testVideo))
     case Method.GET -> !! / "text"  => Http.fromFile(new File("src/main/resources/TestFile.txt"))
   }
 
--- src/main/scala/zutil/StreamHelpers.scala
+++ src/main/scala/zutil/StreamHelpers.scala
@@ -1,15 +1,25 @@
 package zutil
 
 import zio._
-import zio.stream.ZStream
+import zio.stream.{Stream, UStream, ZStream}
 
 import java.time.Duration
 
 object StreamHelpers {
-  def tickStream(size: Long, millis: Int) = ZStream
+  def tickStream(size: Long, millis: Int): UStream[Byte] = ZStream
     .tick(Duration.ofMillis(millis))
     .zipWith(ZStream[Char]('a', 'b', 'c', 'd').repeat(Schedule.forever))((_, c) => c)
     .take(size)
     .map(_.toByte)
     .tap(x => ZIO.logInfo("XXX " + x))
+
+  def sampleQStream[A](initialValue: A): UStream[A] = ZStream.unwrap {
+    Queue.unbounded[A].tap{ q =>
+      ZIO.logInfo(s"Offering initial value: $initialValue") *> q.offer(initialValue)
+    }
+    .map(qStream)
+  }
+
+  def qStream[A](q: Queue[A]): UStream[A] =
+    ZStream.fromQueueWithShutdown(q)
 }
