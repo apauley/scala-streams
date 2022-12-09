f6f2879334fe6dfaaee22133a38654163663964c
Andreas Pauley <apauley@elmar.nl>
2022-12-09 13:17:38 +0100

    Playing with Queue

--- src/main/scala/util/ZHelpers.scala
+++ src/main/scala/util/ZHelpers.scala
@@ -8,23 +8,27 @@ import zio.Duration
 object ZHelpers {
   def tickStream(size: Long, interval: Duration): UStream[Byte] = ZStream
     .tick(interval)
-    .zipWith(ZStream[Char]('a', 'b', 'c', 'd').repeat(Schedule.forever))((_, c) => c)
+    .zipWith(ZStream[Char]('a', 'b', 'c', 'd').repeat(Schedule.forever))(
+      (_, c) => c
+    )
     .take(size)
     .map(_.toByte)
     .tap(x => ZIO.logInfo("XXX " + x))
 
   val randomInts: ZStream[Any, Throwable, Int] = ZStream.repeatZIO {
-    Random.nextInt.tap { i =>
-      ZIO.logInfo(s"$i ") *>
+    Random.nextIntBounded(100).tap { i =>
       Console.print(s"$i ")
     }
   }
 
   def sampleQStream[A](initialValue: A): UStream[A] = ZStream.unwrap {
-    Queue.unbounded[A].tap{ q =>
-      ZIO.logInfo(s"Offering initial value: $initialValue") *> q.offer(initialValue)
-    }
-    .map(qStream)
+    Queue
+      .bounded[A](8)
+      .tap { q =>
+        ZIO.logInfo(s"Offering initial value: $initialValue") *> 
+          q.offer(initialValue)
+      }
+      .map(qStream)
   }
 
   def qStream[A](q: Queue[A]): UStream[A] =
--- src/main/scala/zhttp/FileStreaming.scala
+++ src/main/scala/zhttp/FileStreaming.scala
@@ -14,29 +14,51 @@ object FileStreaming extends ZIOAppDefault {
   val testVideo = "src/main/resources/TestVideoFile.m4v"
 
   // Create HTTP route
-  val app = Http.collectHttp[Request] {
+  val app: Http[Any, Throwable, Request, Response] = Http.collectHttp[Request] {
     case Method.GET -> !! / "health" => Http.ok
 
     // Read the file as ZStream
     // Uses the blocking version of ZStream.fromFile
-    case Method.GET -> !! / "blocking" => Http.fromStream(ZStream.fromPath(Paths.get(testVideo)))
-    case Method.GET -> !! / "filezio" => Http.fromFileZIO(ZIO.succeed(new File(testVideo)))
+    case Method.GET -> !! / "blocking" =>
+      Http.fromStream(ZStream.fromPath(Paths.get(testVideo)))
+    case Method.GET -> !! / "filezio" =>
+      Http.fromFileZIO(ZIO.succeed(new File(testVideo)))
 
-    case Method.GET -> !! / "receive" => Http.fromStream(ZHelpers.tickStream(100L, 100.millis))
-    case Method.GET -> !! / "q" => Http.fromStream(ZHelpers.sampleQStream("Hello bleep"))
-    case Method.GET -> !! / "random" => Http.fromStream(ZHelpers.randomInts.map(_.toString))
-    case Method.GET -> !! / "r100" =>
-      Http.fromStream(ZHelpers.randomInts.take(100).map(_.toString))
+    case Method.GET -> !! / "receive" =>
+      Http.fromStream(ZHelpers.tickStream(1000000L, 1.millis))
 
+    case Method.GET -> !! / "q" =>
+      Http.fromStream {
+        ZStream.unwrap {
+          for {
+            q <- Queue.bounded[String](4096)
+            _ <- AppLogic.doQueueStuff(q).fork
+          } yield ZStream.fromQueueWithShutdown(q)
+        }
+      }
+
+    case Method.GET -> !! / "random" =>
+      Http.fromStream(ZHelpers.randomInts.map(i => s"$i "))
 
     // Uses netty's capability to write file content to the Channel
     // Content-type response headers are automatically identified and added
     // Adds content-length header and does not use Chunked transfer encoding
     case Method.GET -> !! / "video" => Http.fromFile(new File(testVideo))
-    case Method.GET -> !! / "text"  => Http.fromFile(new File("src/main/resources/TestFile.txt"))
+    case Method.GET -> !! / "text" =>
+      Http.fromFile(new File("src/main/resources/TestFile.txt"))
   }
 
   // Run it like any simple app
   val run =
     Server.serve(app).provide(Server.default)
+}
+
+object AppLogic {
+  def doQueueStuff(q: Queue[String]): Task[Unit] = {
+    for {
+      now <- Clock.currentDateTime
+      _ <- q.offer(s"$now\n")
+      _ <- ZIO.sleep(1.milli)
+    } yield ()
+  }.forever
 }
